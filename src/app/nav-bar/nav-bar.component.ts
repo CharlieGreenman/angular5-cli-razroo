3d4bda70ebd72529e58ac3a30399b52766ba3382
Charlie Greenman <charie@razroo.com>
2017-01-04 22:56:08 -0500

    Add async clock to build.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,7 +9,9 @@ import 'rxjs/add/observable/interval';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-  clock = Observable.interval(1000);
+  clock = Observable
+          .interval(1000)
+          .map(()=> new Date ());
 
   constructor() {
 
