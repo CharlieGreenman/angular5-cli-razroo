13d9e309c52725a1f5d36140c0dc0d9d2b8ae9d2
Charlie Greenman <charlie@razroo.com>
2017-01-16 13:09:28 -0500

    Add import for subject for nav-bar component.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -3,6 +3,7 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
 
+import {Subject} from 'rxjs/Subject';
 import {Store} from '@ngrx/store';
 
 @Component({
@@ -14,7 +15,7 @@ export class NavBarComponent implements OnInit {
   click$ = new Subject();
 
   clock;
-  
+
   constructor(store:Store) {
     this.clock = store.select('clock');
 
