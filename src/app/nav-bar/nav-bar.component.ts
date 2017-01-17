aeb3edd703005d68c426284403280d3885a28917
Charlie Greenman <charlie@razroo.com>
2017-01-17 12:45:49 -0500

    Remove strings and replace with constants.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/observable/interval'
-import 'rxjs/add/operator/map'
-import 'rxjs/add/observable/merge'
-import 'rxjs/add/operator/startWith'
-import 'rxjs/add/operator/scan'
-import 'rxjs/add/operator/mapTo'
-import { Subject } from 'rxjs/Subject'
-import { Store } from '@ngrx/store'
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/mapTo';
+import { Subject } from 'rxjs/Subject';
+import { Store } from '@ngrx/store';
+import { SECOND, HOUR } from '../reducers/reducers';
 
 @Component({
   selector: 'app-nav-bar',
@@ -22,8 +23,8 @@ export class NavBarComponent{
       this.clock = store.select('clock')
 
       Observable.merge(
-          this.click$.mapTo('hour'),
-          Observable.interval(1000).mapTo('second')
+          this.click$.mapTo(HOUR),
+          Observable.interval(1000).mapTo(SECOND)
       )
           .subscribe((type)=>{
               store.dispatch({type})
--- src/app/reducers/reducers.ts
+++ src/app/reducers/reducers.ts
@@ -1,11 +1,14 @@
+export const SECOND = 'SECOND';
+export const HOUR = 'HOUR';
+
 export const clock = (state = new Date(), {type}) => {
     const date = new Date(state.getTime())
     switch (type) {
-        case 'second':
+        case SECOND:
             date.setSeconds(date.getSeconds() + 1)
             return date
 
-        case 'hour':
+        case HOUR:
             date.setHours(date.getHours() + 1)
             return date
 
