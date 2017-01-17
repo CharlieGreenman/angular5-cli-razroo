0f0a5e1d4e67dc7b611918187bc35cbbea98f102
Charlie Greenman <charie@razroo.com>
2017-01-17 00:09:14 -0500

    Benchmark progress for rxjs/store.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,28 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/observable/interval';
-import 'rxjs/add/operator/map';
-
-import {Subject} from 'rxjs/Subject';
-import {Store} from '@ngrx/store';
+import 'rxjs/add/observable/interval'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/observable/merge'
+import 'rxjs/add/operator/startWith'
+import 'rxjs/add/operator/scan'
+import 'rxjs/add/operator/mapTo'
+import { Subject } from 'rxjs/Subject'
+import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent{
   click$ = new Subject();
-
   clock;
 
-  constructor(store:Store) {
-    this.clock = store.select('clock');
+  constructor(store: Store<any>) {
+      this.clock = store.select('clock')
 
-  }
-
-  ngOnInit() {
-
+      Observable.merge(
+          this.click$.mapTo('hour'),
+          Observable.interval(1000).mapTo('second')
+      )
+          .subscribe((type)=>{
+              store.dispatch({type})
+          })
   }
 
 }
