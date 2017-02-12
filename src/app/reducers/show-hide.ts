dc9f5c833231b9bb9be1f9c86c2f8fb934d9d207
Charlie Greenman <charie@razroo.com>
2017-02-11 23:20:23 -0500

    issue-4: Fix typo in show-hide html to allow for actiosn to work properly.

--- src/app/reducers/show-hide.ts
+++ src/app/reducers/show-hide.ts
@@ -1,7 +1,7 @@
 import * as types from "../constants/ActionTypes";
 
 const initialState = {
-  show: types.SHOW
+  show: types.HIDE
 };
 
 export const environment = (state = initialState, action) => {
