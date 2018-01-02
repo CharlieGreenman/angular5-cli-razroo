6d91f614e725bf5ca50ad0be1f5e5d6403cab334
Charlie Greenman <charlie@razroo.com>
2018-01-02 16:24:35 -0500

    Add a show-hide module and component.

---
+++ src/app/components/show-hide/show-hide.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShowHideComponent } from './show-hide.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ShowHideComponent]
+})
+export class ShowHideModule { }
