98019f13eece7022cec50e9340634942af332808
Charlie Greenman <charlie@razroo.com>
2018-01-02 14:22:52 -0500

    Add a show-hide-bar module and routing to app.

---
+++ src/app/components/show-hide-bar/show-hide-bar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShowHideBarRoutingModule } from './show-hide-bar-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ShowHideBarRoutingModule
+  ],
+  declarations: []
+})
+export class ShowHideBarModule { }
