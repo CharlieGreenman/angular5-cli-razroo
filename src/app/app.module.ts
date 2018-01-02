ae8faa46178d8f873379271c8f1795c6788f671a
Charlie Greenman <charlie@razroo.com>
2018-01-02 14:05:56 -0500

    Revert "Add product and showHide using the nx ngrx cli/"
    This reverts commit e2ae7ccedb911ed97d08e419ec13a39734c7e87e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,6 @@ import { StoreModule } from '@ngrx/store';
 import { environment } from './reducers/show-hide';
 import { ShowHideBarComponent } from './components/show-hide-bar/show-hide-bar.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { mystateReducer } from './+state/mystate.reducer';
-import { mystateInitialState } from './+state/mystate.init';
-import { MystateEffects } from './+state/mystate.effects';
-import { environment } from '../environments/environment';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { showHideReducer } from './+state/show-hide.reducer';
-import { showHideInitialState } from './+state/show-hide.init';
-import { ShowHideEffects } from './+state/show-hide.effects';
-import { productReducer } from './+state/product.reducer';
-import { productInitialState } from './+state/product.init';
-import { ProductEffects } from './+state/product.effects';
 
 @NgModule({
   declarations: [
@@ -34,17 +22,9 @@ import { ProductEffects } from './+state/product.effects';
     StoreModule.forRoot({environment}),
     StoreDevtoolsModule.instrument({
       maxAge: 5
-    }),
-    StoreModule.forRoot({mystate: mystateReducer}, {initialState: {mystate: mystateInitialState}}),
-    EffectsModule.forRoot([MystateEffects]),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    StoreRouterConnectingModule,
-    StoreModule.forRoot({showHide: showHideReducer}, {initialState: {showHide: showHideInitialState}}),
-    EffectsModule.forRoot([ShowHideEffects]),
-    StoreModule.forFeature('product', productReducer, {initialState: productInitialState}),
-    EffectsModule.forFeature([ProductEffects])
+    })
   ],
-  providers: [MystateEffects, ShowHideEffects, ProductEffects],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
