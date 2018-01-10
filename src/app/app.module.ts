193cb64890f028b5f53fa6bc65a5afa41f87a810
Charlie Greenman <charlie@razroo.com>
2018-01-10 11:17:57 -0600

    Update app to have a Custom Router State Serializer for router-store.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,69 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { Params, RouterStateSnapshot } from '@angular/router';
+import { StoreRouterConnectingModule, routerReducer, RouterReducerState, RouterStateSerializer } from '@ngrx/router-store';
 import { AppComponent } from './app.component';
 
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducerMap } from '@ngrx/store';
 import { environment } from './reducers/show-hide';
 import { ShowHideModule } from './components/show-hide/show-hide.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+export interface RouterStateUrl {
+  url: string;
+  params: Params;
+  queryParams: Params;
+}
+
+export interface State {
+  router: RouterReducerState<RouterStateUrl>;
+  environment: any;
+}
+
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    let route = routerState.root;
+
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+
+    const { url, root: { queryParams } } = routerState;
+    const { params } = route;
+
+    // Only return an object including the URL, params and query params
+    // instead of the entire snapshot
+    return { url, params, queryParams };
+  }
+}
+
+export const reducers: ActionReducerMap<State> = {
+  router: routerReducer,
+  environment
+};
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule,
     ShowHideModule,
-    StoreModule.forRoot({environment, routerReducer: routerReducer}),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
       maxAge: 5
     }),
-    StoreRouterConnectingModule
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router'
+    })
   ],
-  providers: [],
+  providers: [
+    { provide: RouterStateSerializer, useClass: CustomSerializer }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
